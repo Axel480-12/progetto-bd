7f64b6dc7b3f924e9bf32157815e127c3d79b082
Axel480-12 <tris.quattro97@gmail.com>
2026-01-05 11:07:42 +0100

    Aggiunge classe DBConnection

---
+++ src/java/db/DBConnection.java
@@ -0,0 +1,34 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static volatile DBConnection instance;
+    private Connection connection;
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/gestionealloggi?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "3l3.MySQL";
+
+    private DBConnection() throws SQLException {
+        this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if (instance == null) {
+            synchronized (DBConnection.class) {
+                if (instance == null) {
+                    instance = new DBConnection();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
